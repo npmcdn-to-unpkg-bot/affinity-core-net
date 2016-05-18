4bc76714c187143f58c8372c4292e4928fd8ce1f
recapix <renan.leite@globo.com>
2016-05-18 19:50:28 -0300

    Alteracoes

---
+++ wwwroot/App/app.home.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-home',
+  template: '<h2>Home</h2>'
+})
+export class Home implements OnInit {
+    
+    constructor(){
+        
+    }
+    
+     ngOnInit() {
+         
+    } 
+}
---
+++ wwwroot/App/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-dashboard',
+  template: '<h2>Dashboard</h2>'
+})
+export class Dashboard implements OnInit {
+    
+    constructor(){
+        
+    }
+    
+     ngOnInit() {
+         
+    } 
+}
